3eeb4f723553d03ca00409efe9bbbe702dca6183
lkcdm <asyl2008fk@163.com>
2026-01-30 03:37:12 +0800

    Site updated: 2026-01-30 03:37:11

---
+++ assets/back.CB3r2w1A.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.D8U3j-oO.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
